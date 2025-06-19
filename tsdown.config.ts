a54ead2afda247a4fe4d016923141a11d27d286d
Aario <AarioAi@gmail.com>
2025-06-19 18:29:33 +0800

    chore:tsconfig

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,9 +1,12 @@
 import {defineConfig} from 'tsdown'
 
 export default defineConfig({
+    platform: 'browser',
     entry: ['./src', '!**/*.test.{js,ts,tsx}', '!**/*.md'],
     minify: true,
-    // silent: true,
-    platform: 'browser',
-    outDir: 'dist/js',
+    silent: true,
+    outDir: 'dist',
+    dts: {
+        sourcemap: true,
+    }
 })
