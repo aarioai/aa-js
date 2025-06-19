a17a8c4ccd28f4cd7b75bdf28a8e23df4856471d
Aario <AarioAi@gmail.com>
2025-06-20 05:49:58 +0800

    publish to npm

--- demo/vite/src/index.ts
+++ demo/vite/src/index.ts
@@ -3,6 +3,7 @@ import aa from 'aa-js/src/aa.ts'
 (function () {
     console.log("Hello World!")
 
+    aa.http
     aa.http.Get("http://192.168.0.222:8080/v1/users3").then(data => {
         console.log(data)
     })
